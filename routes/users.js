9d642759517783c4170ef630fe3f07c9b88c6ab7
sinharakesh <rksinha.btech@gmail.com>
2025-05-03 11:59:51 +0530

    dataverification added

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 const { readAndNormalizeData } = require("../services/dataProcessor");
 const db = require("../config/pgdb");
+const dataVerification = require("../services/dataVerification");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
@@ -22,6 +23,23 @@ router.get("/list", async function (req, res, next) {
     res.status(500).send("Server error");
   }
   res.end();
+});
+
+router.post("/verification-process", async function (req, res, next) {
+  try {
+    const records = await db.query(
+      "SELECT id,name,mobile,email,dob,is_verified,retry_count FROM users as u where is_verified=false limit 10;"
+    );
+    for (let index = 0; index < records.rows.length; index++) {
+      const verifiedData = await dataVerification(records.rows[index]);
+      console.log(verifiedData);
+    }
+  } catch (err) {
+    console.error(err);
+    // res.status(500).send("Server error");
+  }
+
+  res.send("hello");
 });
 
 module.exports = router;
